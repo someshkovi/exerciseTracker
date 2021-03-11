52ff36bd6300a8ddb392839b83149fd705ea5a20
somesh kovi <someshkovi@gmail.com>
2021-03-11 19:22:38 +0530

    base system with decimal fields added to model

--- exercise/admin.py
+++ exercise/admin.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 from exercise.models import ExerciseTracker, Exercise, BodyPart
 # Register your models here.
 
-@admin.register(ExerciseTracker)
-class ExerciseTrackerAdmin(admin.ModelAdmin):
-    list_display = ('time', 'exercise', 'count')
-    list_filter = ('time', 'exercise')
-
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body_part_primary')
-    list_filter = ('name', 'body_part_primary')
+    list_display = ['name', 'body_part_primary']
+    list_filter = ['name', 'body_part_primary']
 
 admin.site.register(BodyPart)
+
+@admin.register(ExerciseTracker)
+class ExerciseTrackerAdmin(admin.ModelAdmin):
+    list_editable = ['count']
+    list_display = ['time', 'exercise', 'count']
+    list_filter = ['time', 'exercise']
+
--- exercise/models.py
+++ exercise/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
+# from embed_video.fields import EmbedVideoField
 
 # Create your models here.
 class BodyPart(models.Model):
@@ -16,6 +19,7 @@ class Exercise(models.Model):
     body_part_primary = models.ForeignKey(BodyPart, on_delete=models.CASCADE)
     other_body_parts = models.CharField(max_length=200, null=True,blank=True)
     view_link = models.URLField(max_length=500, null=True,blank=True)
+    # video = EmbedVideoField(null=True,blank=True)
 
     class Meta:
         ordering = ['body_part_primary','name']
@@ -24,14 +28,21 @@ class Exercise(models.Model):
         return self.name
 
 class ExerciseTracker(models.Model):
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateField(auto_now_add=True)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    count = models.PositiveIntegerField(null=True, blank=True)
-    repetitions = models.PositiveIntegerField(default=1)
-    weight = models.PositiveIntegerField(null=True, blank=True)
+    count = models.PositiveSmallIntegerField(null=True, blank=True)
+    repetitions = models.PositiveSmallIntegerField(default=1)
+    weight = models.DecimalField(max_digits=4,
+                                    decimal_places=2,
+                                    validators=[MinValueValidator(Decimal('0.01'))],
+                                    null=True,
+                                    blank=True)
     view_link = models.URLField(max_length=500, null=True,blank=True)
-    duration = models.PositiveIntegerField(null=True, blank=True)
-
+    duration = models.DecimalField(max_digits=4,
+                                    decimal_places=2,
+                                    validators=[MinValueValidator(Decimal('0.01'))],
+                                    null=True,
+                                    blank=True)
 
     class Meta:
         ordering = ['time','exercise']
